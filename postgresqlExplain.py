7ecb70f4b41ea27d8b6a3af3b7ae7d9e6c147e77
Miizy <miizyqq@gmail.com>
2024-04-16 11:30:25 +0800

    Adjusted displaying of execution plan

--- postgresqlExplain.py
+++ postgresqlExplain.py
@@ -1,40 +1,38 @@
 import psycopg2
 
 def analyze_query(query):
-    conn = psycopg2.connect("dbname=TPC-H user=postgres password=password")
+    conn = psycopg2.connect("dbname=TPC-H user=postgres password=comsolag")
     cur = conn.cursor()
     
     cur.execute("EXPLAIN (FORMAT JSON) " + query)
     explain_result = cur.fetchone()[0][0]
-    
-    display_query_execution_plan(explain_result)
-    
+    display_plan(explain_result)
+
     cur.close()
     conn.close()
 
-def display_query_execution_plan(explain_result):
-    total_cost = 0
-    plan = explain_result['Plan']
-    plan_seq = traverse_query_execution_plan(plan)
-    for i, subplan in enumerate(plan_seq):
-        total_cost += subplan[1]
-        print(subplan[0] + " | cost: " + str(subplan[1]))
-        if i != len(plan_seq) - 1:
-            print("\t↓")
-    
-    print('\nTotal cost: {:.2f}'.format(total_cost))    
+def display_plan(explain_result):
+    print("Execution Plan:")
+    total_cost = display_subplan(explain_result['Plan'], 0)
+    print('\nTotal cost: {:.2f}'.format(total_cost))
 
-def traverse_query_execution_plan(plan, plan_seq=[]):
-    node = plan.get('Node Type')
-    cost = plan.get('Total Cost', 0)
-
+def display_subplan(plan, indent_level):
+    cost = plan['Total Cost']
+    print("  " * indent_level + plan['Node Type'] + ' - cost: ' + str(plan['Total Cost']))
+    if 'Index Name' in plan:
+        print("  " * (indent_level + 1) + "Index Name:", plan['Index Name'])
+    if 'Relation Name' in plan:
+        print("  " * (indent_level + 1) + "Relation Name:", plan['Relation Name'])
     if 'Plans' in plan:
         for subplan in plan['Plans']:
-            plan_seq = traverse_query_execution_plan(subplan, plan_seq)
-    
-    plan_seq.append((node, cost))
-    return plan_seq
+            cost += display_subplan(subplan, indent_level + 1)
+    return cost
 
-# Example usage
-query = "SELECT * FROM customer WHERE c_mktsegment = 'BUILDING' ORDER BY c_acctbal DESC LIMIT 10;"
+#query = "SELECT * FROM customer WHERE c_mktsegment = 'BUILDING' ORDER BY c_acctbal DESC LIMIT 10;"
+#analyze_query(query)
+
+#query = "SELECT c_phone FROM orders o JOIN customer c ON o.o_custkey = c.c_custkey WHERE c.c_mktsegment = 'FURNITURE';"
+#analyze_query(query)
+
+query = "SELECT c.c_mktsegment, COUNT(*) as order_count FROM orders o JOIN customer c ON o.o_custkey = c.c_custkey GROUP BY c.c_mktsegment;"
 analyze_query(query)
