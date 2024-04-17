fbe50fac80529fcd742cf2c8810f74b746b3a89a
TevinNan <tevinnan@gmail.com>
2024-04-17 23:32:39 +0800

    calculate cost

--- postgresqlExplain.py
+++ postgresqlExplain.py
@@ -1,7 +1,7 @@
 import psycopg2
 
 def analyze_query(query):
-    conn = psycopg2.connect("dbname=TPC-H user=postgres password=comsolag")
+    conn = psycopg2.connect("dbname=Tevin user=postgres password=Tevin123!")
     cur = conn.cursor()
     
     cur.execute("EXPLAIN (FORMAT JSON) " + query)
@@ -28,11 +28,45 @@ def display_subplan(plan, indent_level):
             cost += display_subplan(subplan, indent_level + 1)
     return cost
 
-#query = "SELECT * FROM customer WHERE c_mktsegment = 'BUILDING' ORDER BY c_acctbal DESC LIMIT 10;"
-#analyze_query(query)
+query = "SELECT * FROM customer WHERE c_mktsegment = 'BUILDING' ORDER BY c_acctbal DESC LIMIT 10;"
+analyze_query(query)
 
 #query = "SELECT c_phone FROM orders o JOIN customer c ON o.o_custkey = c.c_custkey WHERE c.c_mktsegment = 'FURNITURE';"
 #analyze_query(query)
 
-query = "SELECT c.c_mktsegment, COUNT(*) as order_count FROM orders o JOIN customer c ON o.o_custkey = c.c_custkey GROUP BY c.c_mktsegment;"
-analyze_query(query)
+# query = "SELECT c.c_mktsegment, COUNT(*) as order_count FROM orders o JOIN customer c ON o.o_custkey = c.c_custkey GROUP BY c.c_mktsegment;"
+# analyze_query(query)
+
+
+import math
+
+def calculateCost(operation, relation_1, relation_2, bufferSize):
+    M = relation_1.numPages # number of blocks
+    N = relation_2.numPages
+    B = bufferSize
+    if operation == 'Seq Scan':
+        # Not sure, cost always 0 when I run the Postgresql explain for squential scan
+        cost = 0
+    elif operation == 'Sort':
+        # from the slides
+        cost = 2*M*(math.log(M)/math.log(B))
+    elif operation == 'Hash':
+        # Since hash is relative to only 1 table
+        cost = 3*M
+    elif operation == 'Hash Join':
+        # from the slides
+        cost = 3*(M + N)
+    # elif operation == "Index Scan":
+        # cost = M/count(distinct(M)) for clustered
+        # cost = numTuple/count(distinct(M))
+    elif operation == 'Merge':
+        # from the slides
+        cost = M+N
+    elif operation == 'Sort Merge Join':
+        # from the slides
+        cost = 3*(M+N)
+    elif operation == 'Block Nested Loop Join':
+        # from the slides
+        cost = M + (M/(B-1))*N
+    
+    return cost
