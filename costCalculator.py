07ca738fe0879eec9d992b429089a99014d90f3e
Miizy <miizyqq@gmail.com>
2024-04-18 20:29:54 +0800

    costCalculator skeleton-ish not sure if correct

--- costCalculator.py
+++ costCalculator.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+
 class CostCalculator:
     def __init__(self, relation_details):
         self.relation_details = relation_details
@@ -13,3 +15,21 @@ class CostCalculator:
         operator = plan['Node Type']
         cost = 0#based on operator do cost estimator
         return cost
+
+class NodeType(ABC):
+    @abstractmethod
+    def calculate_cost(self):
+        pass
+
+class SeqScan(NodeType):
+    def calculate_cost(self, relation_detail):
+        blocks = relation_details.blocks_in_storage
+        return blocks
+
+class IndexScan(NodeType):
+    def calculate_cost(self, relation_detail, index, clustered): 
+        distinct_values = relation_detail.distinct_values[index]
+        if clustered:
+            return relation_detail.blocks_in_storage/distinct_values
+        else:
+            return relation_detail.num_tuples/distinct_values
